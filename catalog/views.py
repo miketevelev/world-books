be6e3aa1aaa4864c9b564711d6ffe7a0ef9bf230
Mike Tevelev <miketevelev@gmail.com>
2023-04-05 21:52:34 +0300

    Index page version 1

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre
 
 def index(request):
-    return HttpResponse('Добро пожаловать на главную страницу')
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact=2).count()
+    num_authors = Author.objects.count()
+
+    return render(request, 'index.html', context={
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_books': num_books},
+    )
--- webbooks/urls.py
+++ webbooks/urls.py
@@ -21,5 +21,5 @@ from catalog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='home'),
+    path('', views.index, name='index'),
 ]
